bd1ade79bf68087337faea43c8a2ea7af64dc133
SMA <sma@wizlinx.com>
2024-11-11 18:02:50 +0500

    Initialize Spring Boot project with SQLite integration and Maven wrapper

---
+++ src/main/java/im/sma/bysma/springbootsqlite/SpringbootSqliteApplication.java
@@ -0,0 +1,13 @@
+package im.sma.bysma.springbootsqlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSqliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSqliteApplication.class, args);
+	}
+
+}
